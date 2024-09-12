b7f20abbfd970be448de436a0114a7fadf447521
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-12 08:04:30 +0200

    Created GET single item handler

--- api/index.js
+++ api/index.js
@@ -8,11 +8,28 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
+// Get all items
 app.get("/", (req, res) => {
-    const jsonItems = fs.readFileSync("./data.json", "utf8");
-    const storedItems = JSON.parse(jsonItems);
-    res.status(200).json(storedItems);
-})
+  const jsonItems = fs.readFileSync("./data.json", "utf8");
+  const storedItems = JSON.parse(jsonItems);
+  res.status(200).json(storedItems);
+});
+
+// Get single item
+app.get("/:itemId", (req, res) => {
+  const jsonItems = fs.readFileSync("./data.json", "utf8");
+  const storedItems = JSON.parse(jsonItems);
+
+  const itemId = req.params.itemId;
+  console.log(storedItems, itemId)
+  const targetItem = storedItems.find((item) => item.id === itemId);
+
+  if (!targetItem) {
+    res.status(404).json({ error: "Could not find the item with given id!" });
+  }
+
+  res.status(200).json(targetItem)
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
