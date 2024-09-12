f5a947055ba37e9c710a57721e9d1f6915eac5df
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-12 07:58:35 +0200

    Created the GET all route handler

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    const jsonItems = fs.readFileSync("./data.json", "utf8");
+    const storedItems = JSON.parse(jsonItems);
+    res.status(200).json(storedItems);
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
